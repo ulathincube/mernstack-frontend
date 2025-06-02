8941ce3aa52e896307f047c1fc54b4d8440bf9a0
Ulathi Ncube <swiftly.graphics@gmail.com>
2025-06-02 18:50:08 +0200

    test get request to backend

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,25 @@
 import { SampleComponent } from '../components';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const Home = () => {
+  const getSampleData = async () => {
+    try {
+      const response = await axios.get(
+        'https://mernstack-backend-qyvi.onrender.com/',
+      );
+
+      const { data } = response;
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getSampleData();
+  }, []);
+
   return (
     <>
       <SampleComponent />
